fbb7d72c805b118cede43c12db9693d4222412a8
PradeepBiligiri <76218503+PradeepBiligiri@users.noreply.github.com>
2023-01-11 10:11:18 +0530

    Add files via upload

---
+++ Create your onw resumew using JSON/script.js
@@ -0,0 +1,59 @@
+let myResume = {
+  basics: {
+    name: "B Pradeep Biligiri",
+    email: "bhadripradeepbiligiri@gmail.com",
+    phone: 8123994997,
+    degree: "M.Sc in Electronics",
+    location: {
+      address: "#27,1st floor,6th cross, Vijay Nagar",
+      postalCode: 570016,
+      city: "Mysore",
+      state: "Karnataka",
+      country: "India",
+    },
+    profiles: [
+      {
+        website: "www.linkedin.com/in/b-pradeep-biligiri-04881b138",
+        github: "https://github.com/PradeepBiligiri",
+      },
+    ],
+  },
+  work: [
+    {
+      company: "Wipro Ltd",
+      position: "Database Administrator",
+      startDate: "2017-10-30",
+      endDate: "present",
+      summary:
+        "i am the database administrator handling various task and gaining lots of experience",
+    },
+  ],
+  education: [
+    {
+      institution: "University of Mysore",
+      department: "Electronics",
+      studyType: "fulltime",
+      "batch start year": 2015,
+      "batch end year": 2017,
+      gpa: 6.8,
+    },
+  ],
+  skills: [
+    {
+      name: "HTML,javascript,CSS,Reactjs",
+      level: "beginer",
+      project: ["Calculator"],
+    },
+  ],
+  languages: [
+    {
+      language: "Kannada,Enlish,Hindi,Tamil",
+    },
+  ],
+  interests: [
+    {
+      name: "Coding",
+    },
+  ],
+};
+console.log(myResume);
